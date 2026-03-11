ef77d45f62a0d04edec3405ca076a69876548633
Janeet <janeet@janjan.8.8.8.8>
2026-03-11 16:39:27 +0000

    added MLHCA MVNN BOCA

--- CA-Solvers/solver/simple-ca-solver.py
+++ CA-Solvers/solver/simple-ca-solver.py
@@ -2,13 +2,15 @@
 from pulp import *
 
 # Quick Functions:
-add_constraints = lambda problem, constraints: [problem.addConstraint(c) for c in constraints]
+add_constraints = lambda problem, constraints: [
+    problem.addConstraint(c) for c in constraints
+]
 
 # Parse File
 with open("../../CATS/0000.txt", "r") as file:
     data = file.read()
 
-data = list(filter(lambda x: '\t' in x ,data.split("\n")))
+data = list(filter(lambda x: "\t" in x, data.split("\n")))
 data = list(map(lambda x: x.split("\t"), data))
 bid_val = {i[0]: i[1] for i in data}
 bid_items = {i[0]: i[2:-1] for i in data}
@@ -22,9 +24,11 @@ for bid, items in bid_items.items():
         item_overlap[item].append(bid)
 
 # Build Up LP
-bid_vars = {i: LpVariable(f"bid_{i}",0 , 1, LpInteger) for i in bid_val.keys()}
+bid_vars = {i: LpVariable(f"bid_{i}", 0, 1, LpInteger) for i in bid_val.keys()}
 
-non_overlaping_constraints = [lpSum([bid_vars[i] for i in bids]) <= 1  for item, bids in item_overlap.items()]
+non_overlaping_constraints = [
+    lpSum([bid_vars[i] for i in bids]) <= 1 for item, bids in item_overlap.items()
+]
 
 obj_function = lpSum([bid_vars[i] * float(bid_val[i]) for i in bid_val.keys()])
 
@@ -46,20 +50,20 @@ for v in problem.variables():
 
 
 # Equivalent. Check if the LP file created by CATs is the same as the one our script did.
-#from docplex.mp.model_reader import ModelReader
+# from docplex.mp.model_reader import ModelReader
 #
 #
-#m = ModelReader.read("../../CATS/0000.lp")
-#m.export_as_mps("./0000.mps")
+# m = ModelReader.read("../../CATS/0000.lp")
+# m.export_as_mps("./0000.mps")
 #
-#variables, pulp_model = LpProblem.fromMPS("./0000.mps")
+# variables, pulp_model = LpProblem.fromMPS("./0000.mps")
 #
 ## You can now work with the pulp_model object as usual
-#print(f"Successfully read problem: {pulp_model.name}")
+# print(f"Successfully read problem: {pulp_model.name}")
 #
-#pulp_model.solve()
+# pulp_model.solve()
 #
 #
-#for v in pulp_model.variables():
+# for v in pulp_model.variables():
 #    if v.varValue == 1:
 #        print(v.name, "=", v.varValue)
